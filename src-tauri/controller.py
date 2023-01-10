8d73501ad770ede707e96cc6448f268d083ce6de
harjeb <juchenjun@uniontech.com>
2023-01-10 09:53:05 +0800

    chore(): add sleep log

--- src-tauri/controller.py
+++ src-tauri/controller.py
@@ -11,7 +11,11 @@ ROOT_PATH = os.path.abspath('.')
 def check_sleep(_settings_dict):
     if _settings_dict["game"]["farmingMode"] == "Take a break":
         mins = _settings_dict["game"]["itemAmount"]
-        time.sleep(60*mins)
+        sleep_time = 60*mins
+        print("=====开始休息=====")
+        for i in range(sleep_time):
+            time.sleep(1)
+            print("need sleep %d" % (sleep_time-i))
         return True
     else:
         return False
@@ -35,8 +39,10 @@ for i in _list:
                     if os.path.exists(newdir+'settings.json'):
                         os.remove(newdir+'settings.json')
                     os.rename(_file,newdir+'settings.json')
-                except:
+                except Exception as e:
                     print("没找到正确任务配置")
+                    print(e)
+                    print("================")
                     break
                 _settings = open(f"{ROOT_PATH}/backend/settings.json",encoding='utf-8')
                 if not check_sleep(json.load(_settings)):
